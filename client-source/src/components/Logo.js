8c2d3b7433ab964c489cbf314f4896b5c923580f
David Poindexter <drpoindexter@gmail.com>
2019-03-24 15:11:16 -0400

    Add logo component, introduce pattern for component-based image queries

---
+++ client-source/src/components/Logo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const BaseLogo = ({ data, className }) => (
+  <div className={className}>
+    <Img fixed={data.logo.childImageSharp.fixed} />
+  </div>
+);
+
+const Logo = styled(BaseLogo)`
+  border: 1px solid red;
+`;
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        logo: file(relativePath: { eq: "indy_steel_logo.png" }) {
+          childImageSharp {
+            fixed {
+              ...GatsbyImageSharpFixed_noBase64
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Logo data={data} {...props} />}
+  />
+);
